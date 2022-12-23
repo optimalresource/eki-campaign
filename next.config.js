4d78815bd5f32e2a7832e3e5d6300116971faf4f
optimalresource <joptimalresource@live.com>
2022-12-23 21:00:14 +0100

    More changes.

--- next.config.js
+++ next.config.js
@@ -2,8 +2,8 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    loader: "akamai",
-    path: "https://eki-market.web.app/assets/",
+    loader: "imgix",
+    path: "https://eki-market.web.app",
   },
 };
 
